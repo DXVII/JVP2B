e724a8093e85684763f2bcdb5a3602983d782314
DXVII <david.pham.d@gmail.com>
2017-10-20 18:52:46 +1100

    java doc, world and app done

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@
 /**
  * Project skeleton for SWEN20003: Object Oriented Software Development 2017
  * by Eleanor McMurtry
+ * edited by David Pham (756598) for Project 2B of OOSD 2017
  */
 import java.io.FileNotFoundException;
 import org.newdawn.slick.AppGameContainer;
@@ -21,22 +22,31 @@ public class App extends BasicGame {
     public static final int TILE_SIZE = 32;
     public static final int TXT_BUFF = 30;
 
-    //level data
+    //File Paths
     public static final String LVL_TXT = "levels/";
 	public static final String LVL_TXT_END =".lvl";
 
     public static final String IMG_TXT = "res/";
 	public static final String IMG_TXT_END =".png";
 
+    // key app attirbutes
     public static final int MAX_LVL = 5;
 
+    // static used as only one instance of App is used
+    // app controls the world
     private static World world;
+    //path where the level to be loaded can be found
     private static String lvlAddress = "";
+    // current level tracked
     private static int nLvl = 0;
 
     public App() {
       super("Shadow Blocks");
     }
+    /** INIT initialises the game.
+     * @param gc The Slick game container object.
+     * @throws SlickException if it fails
+     */
 
     @Override
     public void init(GameContainer gc) throws SlickException {
@@ -49,38 +59,10 @@ public class App extends BasicGame {
 		}
     }
 
-
-///////////////////////////////////////////////////////////////////////////////
-
-    public static void reset() throws FileNotFoundException, SlickException{
-		App.lvlAddress = App.LVL_TXT + Integer.toString(App.nLvl) + App.LVL_TXT_END;
-        App.world = new World(App.lvlAddress);
-        MoveStack.clearStack();
-	}
-
-    public static void nextLevel() throws FileNotFoundException, SlickException{
-        if(App.nLvl < App.MAX_LVL){
-            App.nLvl += 1;
-        }
-        App.reset();
-    }
-    
-    public static void prevLevel() throws FileNotFoundException, SlickException{
-        if(App.nLvl > 0){
-            App.nLvl -= 1;
-        }
-        App.reset();
-    }
-
-
-
-
-
-//////////////////////////////////////////////////////////////////////////////
-
     /** UPDATE the game state for a frame.
      * @param gc The Slick game container object.
      * @param delta Time passed since last frame (milliseconds).
+     * @throws SlickException if it fails
      */
     @Override
     public void update(GameContainer gc, int delta) throws SlickException {
@@ -100,7 +82,6 @@ public class App extends BasicGame {
 
     }
 
-
     /** RENDER the entire screen, so it reflects the current game state.
      * @param gc The Slick game container object.
      * @param g The Slick graphics object, used for drawing.
@@ -108,7 +89,32 @@ public class App extends BasicGame {
     public void render(GameContainer gc, Graphics g) throws SlickException {
     	world.render(g);
     }
+    //////////////////////Not form Original Skeleton///////////////////////////
+    /** RESET game, loading the level from scratch.
+     */
+    public static void reset() throws FileNotFoundException, SlickException{
+		App.lvlAddress=App.LVL_TXT+Integer.toString(App.nLvl)+App.LVL_TXT_END;
+        App.world = new World(App.lvlAddress);
+        MoveStack.clearStack();
+	}
+    /** NEXTLEVEL loads the next level
+    */
+    public static void nextLevel() throws FileNotFoundException, SlickException{
+        if(App.nLvl < App.MAX_LVL){
+            App.nLvl += 1;
+        }
+        App.reset();
+    }
+    /** PREVLEVEL loads the previous level
+    */
+    public static void prevLevel() throws FileNotFoundException, SlickException{
+        if(App.nLvl > 0){
+            App.nLvl -= 1;
+        }
+        App.reset();
+    }
 
+    //////////////////////////////////////////////////////////////////////////////
 
     /** Start-up method. Creates the game and runs it.
      * @param args Command-line arguments (ignored).
--- src/World.java
+++ src/World.java
@@ -1,30 +1,45 @@
+//David Pham 756598
+
+/** World
+ * holds the game world and all worldly actions and states'
+ * most sprites communicate through the world
+ */
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
 public class World {
-	//Direction
+//Directions
 	public static final int UP = 0;
 	public static final int DOWN = 1;
 	public static final int LEFT = 2;
 	public static final int RIGHT = 3;
 
+// Game stuff
+	//level dimensions of the current world
 	private int lvlWidth;
 	private int lvlHeight;
-	private int nMoves = 0;
-	private ArrayList<Sprite> spriteArray;
+	// compare number of targets covered to number of targets in world to win
 	private int nTargets = 0;
 	private int nTargetsCov = 0;
-	private Sprite door;
-	//passed to enemy units
-	private boolean playerMoved = false;
+	//number of moves a player has made, undoing reduces this
+	private int nMoves = 0;
 
-	//questionable if needed
+//Sprite stuff
+	// where all the sprites live
+	private ArrayList<Sprite> spriteArray;
+	// door sprite to be easily accessed by switch
+	private Sprite door;
+
+//Player stuff
+	//rogue and mage be watching
+	private boolean playerMoved = false;
+	// player is easier to stalk
 	private Position currPlayPos;
+	// intermediary variable showing all sprites of at a requested location
 	private ArrayList<Sprite> samePosSprites;
 
 
@@ -32,16 +47,20 @@ public class World {
 		this.spriteArray = Loader.loadSprites(this,lvlAddress);
 		this.findDoor();
 	}
+/** UPDATE, updates the world state based on class and human keyboard events
+ * @param input input from keyboard
+ * @param delta time since last update
+ */
+	//@Override
+	public void update(Input input, int delta)
+	throws FileNotFoundException, SlickException {
 
-
-	public void update(Input input, int delta) throws FileNotFoundException, SlickException {
-
-	//Restart
+	//Restart Command
 		if(input.isKeyPressed(Input.KEY_R)) {
 			App.reset();
 		}
 
-	//Undo
+	//Undo Command
 		else if(input.isKeyPressed(Input.KEY_Z)) {
 			MoveStack.undoMoves();
 			if(this.nMoves > 0){
@@ -49,28 +68,32 @@ public class World {
 			}
 		}
 
-	// Next Level
+	// Next Level Command
 		else if(input.isKeyPressed(Input.KEY_N)){
 			App.nextLevel();
 		}
+	//Previous Level Command
 		else if(input.isKeyPressed(Input.KEY_B)) {
 			App.prevLevel();
 		}
 
 
-	//Non-system command
+	//Non-system related commands
 		else {
+		//Check win
 			if(this.nTargets == this.nTargetsCov){
 				App.nextLevel();
 			}
+		//Looking at all the Sprites
 			// C style for loop (incase objects get removed)//
 			for(int i=0; i<spriteArray.size(); i++) {
 				Sprite currSpr = spriteArray.get(i);
 
 
-			// Time ticks & time based movement
+			// Time ticks & time based movement //
 				// Skeleton
 				if(currSpr instanceof Skeleton){
+					//moves if a time limit is hit
 					((Skeleton) currSpr).update(this, delta);
 				}
 				// Ice
@@ -86,14 +109,16 @@ public class World {
 				}
 
 
-			// Check collision events
-				
-				//Player Death
+			// Check collision events //
+
+				//Player Death when Enemy matches player position
+				// don't let player die if no move attempt made :)
 				if(currPlayPos != null && currSpr instanceof Enemy &&
 				((currSpr.getPosition()).equals(this.currPlayPos)) ) {
+					// reset level upon death
 					App.reset();
 				}
-				
+
 				//Target Dynamics
 				if(currSpr instanceof Target){
 					//Target checks sprites at its position
@@ -105,73 +130,79 @@ public class World {
 							//and suddenly sees block, target +1
 							if(checkSpr instanceof Block){
 								nTargetsCov+=1;
-								// check if you've won the game
 								//block is now covered
 								((Target) currSpr).cover();
 								break;
 							}
 						//if was blocked,
 						} else {
-
 							//  and still sees block end loop
 							if(checkSpr instanceof Block){
 								/*nothing special*/
 								break;
 							}
 
-							// and now no blocks detected->it has been removed
+							// and now no blocks detected -> it has been removed
 							nTargetsCov-=1;
 							((Target) currSpr).uncover();
 						}
 
 					}// target loop
 
-
 				}// target conditions
 
 
-				// switch and door dynamics
+				// Switch and Door dynamics
 				if(currSpr instanceof Switch) {
+					//obtain all sprites on switch and check if block
 					this.samePosSprites = getSpritesAt(currSpr.getPosition());
 					for(Sprite checkSpr : this.samePosSprites){
+						//ooh a block, tell door to hide or.supress()!
 						if(checkSpr instanceof Block){
 							((Door) this.door).doorOpen();
 							break;
 						}
+						//no block, stop hiding door
 						((Door) this.door).doorClose();
 
 					}
 				}
 
-				// block movement: ice, tnt, stone
+				// Block movement: Ice, Tnt, Stone
 				if(currSpr instanceof Block) {
+					//if something has collided into block, they need to move
 					this.samePosSprites = getSpritesAt(currSpr.getPosition());
-
 					for(Sprite checkSpr : this.samePosSprites) {
-						// collision only with rogue and player
+						// collisions only with rogue and player
+						// block to move in the same direction they were moving
 						if (checkSpr instanceof Player) {
-							((Block) currSpr).move(this, checkSpr.getDirection());
+							((Block)currSpr).move(this,checkSpr.getDirection());
 						}
 						else if (checkSpr instanceof Rogue) {
-							((Block) currSpr).move(this, checkSpr.getDirection());
+							((Block)currSpr).move(this,checkSpr.getDirection());
 						}
 					}
 				}
 
-				//tnt explosion dynamics
+				//Tnt, Cracked (wall) and Explosion dynamics
 				if(currSpr instanceof Cracked) {
 					this.samePosSprites = getSpritesAt(currSpr.getPosition());
 					for(Sprite checkSpr : this.samePosSprites) {
+						// TNT meet Cracked (wall)
 						if(checkSpr instanceof Tnt) {
+							//first you explode
 							((Tnt) checkSpr).explode(this);
+							// then you explode
 							((Cracked) currSpr).explode(this);
 						}
 					}
 				}
 
 				//Player Movement
+				// mage and rogue need to know if player has moved since
 				this.playerMoved = false;
 				if(currSpr instanceof Player) {
+					//if moved, update playerMoved, invoke move
 					if(input.isKeyPressed(Input.KEY_UP)) {
 						this.playerMoved = true;
 						this.nMoves += 1;
@@ -193,16 +224,15 @@ public class World {
 						this.nMoves += 1;
 						((Player) currSpr).move(this, World.RIGHT);
 					}
-					//player movement easily access
+					//player location easily accessed
 					currPlayPos = currSpr.getPosition();
 				}
-				
-				
-				//player observing enemy movement
+
+
+				//Mage an Rogue are stalkers, move when player has
 				if(this.playerMoved){
 					for(Sprite enemySpr : spriteArray) {
-						if(enemySpr instanceof Rogue ||
-						enemySpr instanceof Mage){
+						if(enemySpr instanceof Rogue||enemySpr instanceof Mage){
 							((Enemy) enemySpr).move(this);
 						}
 					}
@@ -214,33 +244,41 @@ public class World {
 	}//update
 
 
-///////////////////////////////////////////////////////////////////////////////
+	/** Render floor then player to enure player is always after
+	 * @param g graphics
+	 */
 
-	// render floor then player to enure player is always after
+	//@Override
 	public void render(Graphics g) {
 		for (Sprite sprite : this.spriteArray) {
 			sprite.getWorldDim(this);
 			sprite.render(g);
 		}
-		g.drawString("Moves made: "+Integer.toString(nMoves), 
+		g.drawString("Moves made: "+Integer.toString(nMoves),
 				App.TXT_BUFF, App.SCREEN_HEIGHT-App.TXT_BUFF);
 	}
 
 
-///////////////////////////////////////////////////////////////////////////////
+////////////////////////// Special Methods /////////////////////////////////////
 
+	/** getSpritesAt: retrieves sprites at a given location
+	 * @param position takes a position to check sprites
+	*/
 	public ArrayList<Sprite> getSpritesAt(Position position){
+		//make a new ArrayList of sprites to return
 		ArrayList<Sprite> list = new ArrayList<Sprite>();
 		for(Sprite currSpr : this.spriteArray){
 			if(currSpr.getPosition().equals(position)){
+				//appends all sprites at a given location
 				list.add(currSpr);
 			}
 		}
 		return list;
 	}
-
+	/** addExplosion: makes an explosion appear at a given location
+	 * @param position takes a position to put explosion
+	*/
 	public void addExplosion(Position position) throws SlickException{
-		//add path for explosion pic
 		this.spriteArray.add(new Explosion("explosion", position));
 	}
 
